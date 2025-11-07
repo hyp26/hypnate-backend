525dea49ff6021e81975e448db9a0135591cea9e
hyp26 <hypnate.2026@gmail.com>
2025-11-07 12:04:13 +0530

    Product & Order CRUD endpoints

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getAllOrders, createOrder } from "../controllers/order.controller";
+import { createOrder, getOrders, getOrderById, updateOrderStatus, getOrderStatus } from "../controllers/order.controller";
+import { verifyToken } from "../middleware/authMiddleware";
 
 const router = Router();
+router.use(verifyToken);
 
-router.get("/", getAllOrders);
+router.get("/", getOrders);
 router.post("/", createOrder);
+router.get("/:id", getOrderById);
+router.put("/:id/status", updateOrderStatus);
+router.get("/:id/status", getOrderStatus);
+
 
 export default router;
